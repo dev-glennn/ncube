cd3bc53c0c0d2315a90807d1d30113034810b3dc
yoonvelop <yoonvelop@gmail.com>
2020-03-05 11:25:13 +0900

    feat: header (nav만) 추가
    - 스크롤 제일 위는 투명 버전
    - 기본 헤더 추가

--- html/include/header.php
+++ html/include/header.php
@@ -1,9 +1,31 @@
 <?php
 include_once('head.sub.php');
+add_javascript('<script type="text/javascript" src="' . $NC_PATH . '/js/header.js"></script>');
 ?>
-<header>
+<header class="start">
     <div class="wrap">
-        header
+        <a class="header-logo" href="/"></a>
+        <nav class="header__nav">
+            <ul>
+                <li data-target="#story" class="select"><a>STORY</a></li>
+                <li data-target="#philosophy"><a>PHILOSOPHY</a></li>
+                <li data-target="#ecosystem"><a>ECOSYSTEM</a></li>
+                <li data-target="#vision"><a>VISION</a></li>
+                <li data-target="#contact"><a>CONTACT</a></li>
+            </ul>
+        </nav>
+        <div class="header__lang">
+            <ul>
+                <li><a href="">JOIN +</a></li>
+                <li class="lang__change"><?= strtoupper($country) ?>
+                    <ul class="change__ul">
+                        <li><a href="#" data-lang="en">EN</a></li>
+                        <li><a href="#" data-lang="ch">CH</a></li>
+                        <li><a href="#" data-lang="ko">KO</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
 </header>
 
