935b02ec8389ee7810093a9c0b43ba75ecdbfe5a
yoonvelop <yoonvelop@gmail.com>
2020-03-05 17:19:38 +0900

    style: story 변경
    - h1 타이틀 추가
    - 내용, 이미지 변경
    - margin, padding 변경

--- html/story.php
+++ html/story.php
@@ -4,27 +4,30 @@ add_stylesheet("<link rel='stylesheet' href='" . $NC_PATH . "/css/story.css'>");
 
 <section id="story">
     <div class="wrap">
-        <div class="story__image">
-            <img src="/ncube/images/story-main.png" alt="story image">
-        </div>
-        <div class="story__contents">
-            <h1>STORY</h1>
-            <p>앤큐브 홀딩스는 산하 핵심계열사 및 제휴법인과의 융합을 통해 사람 ‘人’
-                공통체를 기반으로 실질적 Tokenomics 글로벌 생태계 기반 구축을
-                목표로 블록체인 기술 개발 및 공유경제 리더기업을 육성하고, 글로벌의
-                천만명의 건강한 앤큐브‘人’ 공동체 생태계 완성을 목표 달려갑니다.
-                <br/><br/>
-                앤큐브의 가치는 새로운 유통의 흐름과 중심을 사람 ‘人’으로 그 가치를
-                추구하며 사람으로 이루어진 새로운 공동체의 패러다임을 선도합니다.
-            </p>
-            <div class="circle-box">
-                <div class="circle">
-                    <span>The Frontier of 4th <br/> Industrial Revolution</span>
-                    <p>글로벌 유통혁신 커뮤니티 <br/> (공동체) </p>
-                </div>
-                <div class="circle">
-                    <span>nCube humunity <br/> Community</span>
-                    <p>앤큐브 人 공동체 <br/> (휴머니티)</p>
+        <h1>STORY</h1>
+        <div>
+            <div class="story__image">
+                <img src="/ncube/images/story-main.png" alt="story image">
+            </div>
+            <div class="story__contents">
+                <h1>nCube Committee</h1>
+                <p>앤큐브 홀딩스는 산하 핵심계열사 및 제휴법인과의 융합을 통해 사람 ‘人’
+                    공통체를 기반으로 실질적 Tokenomics 글로벌 생태계 기반 구축을
+                    목표로 블록체인 기술 개발 및 공유경제 리더기업을 육성하고, 글로벌의
+                    천만명의 건강한 앤큐브‘人’ 공동체 생태계 완성을 목표 달려갑니다.
+                    <br/><br/>
+                    앤큐브의 가치는 새로운 유통의 흐름과 중심을 사람 ‘人’으로 그 가치를
+                    추구하며 사람으로 이루어진 새로운 공동체의 패러다임을 선도합니다.
+                </p>
+                <div class="circle-box">
+                    <div class="circle">
+                        <span>The Frontier of 4th <br/> Industrial Revolution</span>
+                        <p>글로벌 유통혁신 커뮤니티 <br/> (공동체) </p>
+                    </div>
+                    <div class="circle">
+                        <span>nCube humunity <br/> Community</span>
+                        <p>앤큐브 人 공동체 <br/> (휴머니티)</p>
+                    </div>
                 </div>
             </div>
         </div>
