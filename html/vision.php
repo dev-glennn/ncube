d2c375d0041c24c9450c8c495f1236d9261a80fb
dev-glennn <dev.glennn@gmail.com>
2020-03-05 13:59:31 +0900

    feat: vision 1차 퍼블리싱
    - vision 영역 1차 퍼블리싱
    - contact text shadow 삭제

--- html/vision.php
+++ html/vision.php
@@ -1,5 +1,92 @@
+<?php
+add_stylesheet("<link rel='stylesheet' href='" . $NC_PATH . "/css/vision.css?ver=" . TW_CSS_VER . "'>");
+?>
+
 <section id="vision">
-    <div class="wrap">
-        vision
+    <h2 class="vision-title">VISION</h2>
+    <div class="table">
+        <div class="table-cell">
+            <div class="wrap">
+                <div class="vision-main">
+                    <div class="section-vision-circle">
+                        <div class="vision-half-circle">
+                            <div class="ball soft-color location-0">
+                                <p class="vision-text">
+                                    hudex Exchange<br>
+                                    Blockchain Incubating LAB
+                                </p>
+                            </div>
+                            <div class="ball hard-color location-25">
+                                <p class="vision-text">
+                                    Law Firm
+                                    <span>(법무법인)</span>
+                                </p>
+                            </div>
+                            <div class="ball soft-color location-50">
+                                <p class="vision-text">
+                                    Holdings Group
+                                    <span>(지주회사:그룹)</span>
+                                </p>
+                            </div>
+                            <div class="ball hard-color location-75">
+                                <p class="vision-text">
+                                    Accounting Firm
+                                    <span>(회계법인)</span>
+                                </p>
+                            </div>
+                            <div class="ball soft-color location-100">
+                                <p class="vision-text">
+                                    Bigdata System Control<br>
+                                    Academy Center
+                                </p>
+                            </div>
+                        </div>
+                        <div class="b-vision-circle">
+                            <div class="s-vision-circle">
+                                <img class="s-vision-logo" src="<?= $NC_PATH ?>/images/logo-b.png">
+                                <p>
+                                    <span>nCube Committee</span>
+                                    <span>(앤큐브 운영 위원회)</span>
+                                </p>
+                                <div class="vision-arrow">
+                                    <img src="<?= $NC_PATH ?>/images/vision-arrow.png" width="22">
+                                    <img src="<?= $NC_PATH ?>/images/vision-arrow.png" width="22">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="vision-circle-result">
+                            hucamp
+                            <span>(재단법인 설립)</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="vision-desc">
+                    <ul>
+                        <li>· humall (쇼핑몰)</li>
+                        <li>· SNS (hutalk)</li>
+                    </ul>
+                    <ul>
+                        <li>· e-Game (인터넷 게임)</li>
+                        <li>· e-C / Trading (코인 트레이딩)</li>
+                        <li>· e-Insurance (인터넷 보험)</li>
+                    </ul>
+                    <ul>
+                        <li>· Distributing Company (유통법인)</li>
+                        <li>· Cubezone Group (프랜차이즈 법인)</li>
+                    </ul>
+                    <ul>
+                        <li>· Real-estate (부동산)</li>
+                        <li>· Manufacturing (제조)</li>
+                        <li>· Tour (여행)</li>
+                        <li>· HOTEL (호텔)</li>
+                    </ul>
+                    <ul>
+                        <li>· Art (예술)</li>
+                        <li>· Entertainment (연예)</li>
+                        <li>· Sports (스포츠)</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
