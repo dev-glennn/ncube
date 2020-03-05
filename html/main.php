821fa829cda919d6609894547e0cbc5b4c068ce2
yoonvelop <yoonvelop@gmail.com>
2020-03-05 13:55:46 +0900

    feat: main
    - 메인 슬라이드 추가
    - 네비 추가
    - 이미지 01버전만 세개임

--- html/main.php
+++ html/main.php
@@ -1,5 +1,49 @@
+<?php
+add_stylesheet("<link rel='stylesheet' href='" . $NC_PATH . "/css/main.css'>");
+add_javascript('<script type="text/javascript" src="' . $NC_PATH . '/js/tw.slide.js"></script>');
+?>
 <section id="main">
-    <div class="wrap">
-        main
-    </div>
+    <ul>
+        <li>
+            <div>
+                <img src="/ncube/images/main-bg01.png" alt="main01">
+                <div class="main-box">
+                    <img src="/ncube/images/main-icon01.png" alt="Humunity">
+                    <h1>HUMUNITY</h1>
+                    <p>새로운 흐름, 선도적 경제, 도전적‘人’공동체</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div>
+                <img src="/ncube/images/main-bg01.png" alt="main02">
+                <div class="main-box">
+                    <img src="/ncube/images/main-icon01.png" alt="Humunity">
+                    <h1>HUMUNITY</h1>
+                    <p>새로운 흐름, 선도적 경제, 도전적‘人’공동체</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div>
+                <img src="/ncube/images/main-bg01.png" alt="main03">
+                <div class="main-box">
+                    <img src="/ncube/images/main-icon01.png" alt="Humunity">
+                    <h1>HUMUNITY</h1>
+                    <p>새로운 흐름, 선도적 경제, 도전적‘人’공동체</p>
+                </div>
+            </div>
+        </li>
+    </ul>
 </section>
+<script>
+    $('#main').tw_slide({
+        mode:'basic',
+        loop:true,
+        auto:true,
+        pause:".pause-div",
+        speed:500,
+        time:4500,
+        navi:true
+    });
+</script>
