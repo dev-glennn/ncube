a506685e341682d04817d15bade0c368369d0507
dev-glennn <dev.glennn@gmail.com>
2020-03-06 11:44:20 +0900

    feat: ecosystem 일본어 번역 추가

--- html/ecosystem.php
+++ html/ecosystem.php
@@ -21,19 +21,19 @@ add_stylesheet("<link rel='stylesheet' href='" . $NC_PATH . "/css/ecosystem.css?
                 <div class="content-bottom">
                     <a class="bottom-item" target="_blank" href="http://humall.kr/shop/">
                         <img src="/ncube/images/eco-humall.png" alt="humall">
-                        <span><?= lang('온라인쇼핑몰', 'Online shopping mall', '网上购物中心') ?></span>
+                        <span><?= lang('온라인쇼핑몰', 'Online shopping mall', '网上购物中心', 'オンラインショッピングモール') ?></span>
                     </a>
                     <a class="bottom-item">
                         <img src="/ncube/images/eco-cube.png" alt="cubezone">
-                        <span><?= lang('프렌차이즈 Biz', 'Friends Biz', 'Frenchiz Biz') ?></span>
+                        <span><?= lang('프렌차이즈 Biz', 'Friends Biz', 'Frenchiz Biz', 'フランチャイズBiz') ?></span>
                     </a>
                     <a class="bottom-item" target="_blank" href="http://hustory.net/">
                         <img src="/ncube/images/eco-hustory.png" alt="hustory">
-                        <span><?= lang('비지니스 오피스', 'Business Office', '商务办公') ?></span>
+                        <span><?= lang('비지니스 오피스', 'Business Office', '商务办公', 'ビジネス·オフィス') ?></span>
                     </a>
                     <a class="bottom-item">
                         <img src="/ncube/images/eco-huwallet.png" alt="huwallet">
-                        <span><?= lang('전자지갑', 'Electronic wallet', '电子钱包') ?></span>
+                        <span><?= lang('전자지갑', 'Electronic wallet', '电子钱包', '電子財布') ?></span>
                     </a>
                 </div>
             </div>
