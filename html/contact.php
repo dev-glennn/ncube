83afdd8b23b3643ff77ce8233e0288855e998896
dev-glennn <dev.glennn@gmail.com>
2020-03-06 10:28:51 +0900

    feat: contact 번역

--- html/contact.php
+++ html/contact.php
@@ -8,6 +8,6 @@ add_stylesheet("<link rel='stylesheet' href='" . $NC_PATH . "/css/contact.css'>"
         <iframe class="ifr-contact-address"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3165.0374555631206!2d127.05082451566113!3d37.50703473529059!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca412293b4333%3A0x7bfaec458a2d36e!2zKOyjvCnrqZTrlJTsiqTtgJjslrQ!5e0!3m2!1sko!2skr!4v1583372579948!5m2!1sko!2skr"
                 frameborder="0"></iframe>
-        <p class="contact-address">A. 서울 강남구 삼성로 95길 메디스퀘어빌딩 2,3층</p>
+        <p class="contact-address">A. <?=lang('서울 강남구 삼성로 95길 메디스퀘어빌딩 2,3층','F2-F3, Medisquare Building, 35, Samseong-ro 95-gil, Gangnam-gu, Seoul, Republic of Korea','首尔江南区三星路95街Medisquare大厦F2,F3')?></p>
     </div>
 </section>
